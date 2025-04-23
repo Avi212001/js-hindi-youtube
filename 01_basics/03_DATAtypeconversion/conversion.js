9d817505a59deb4ebd4df87cf023384c1d053083
Avi212001 <142044743+Avi212001@users.noreply.github.com>
2025-04-23 15:21:29 +0000

    conversion

---
+++ 01_basics/03_DATAtypeconversion/conversion.js
@@ -0,0 +1,24 @@
+let a = null;
+
+console.log(a);
+
+
+console.log(typeof(a));
+
+let c = Number(a);
+console.log(typeof c);
+console.log(c);
+
+//nan means not a number 
+// when we take true it convert into 1 and wwhen it take false it convert into 0 
+
+
+let d = "gger";
+
+let e = Boolean(d);
+
+console.log(e);
+
+//when we see convert something into bollean in js we see there is a lot of case 
+// a) when i pass 1 it give true and when i pass 0 it give false 
+//b) when i pass empty string it will give false and when i pass to my name as a string then it give true 
